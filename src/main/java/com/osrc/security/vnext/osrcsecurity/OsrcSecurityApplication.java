befcbc281376039e7722fc543afd35d1004456c5
Clayton James <clayton_jms@yahoo.co.uk>
2019-03-03 22:49:25 -0500

    added initial files

---
+++ src/main/java/com/osrc/security/vnext/osrcsecurity/OsrcSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.osrc.security.vnext.osrcsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrcSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsrcSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/osrc/security/vnext/osrcsecurity/OsrcSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.osrc.security.vnext.osrcsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OsrcSecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
